85595a4c703792b76b4dbf8ffcf323722fa386b4
Patrick Walker <41069751+SouthUniform7@users.noreply.github.com>
2022-02-21 21:22:37 -0500

    added build

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 import Resume from './Resume.pdf'
